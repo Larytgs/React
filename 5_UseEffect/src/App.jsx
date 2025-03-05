fcbaed855589c8a17258432d2eed6697e2f254a7
laryTgs <laryssatortato99@gmail.com>
2025-03-05 16:04:25 -0300

    att

---
+++ 5_UseEffect/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import UseEffect from './componente/UseEffect'
+function App() {
+  
+
+  return (
+    <>
+    <h1>Projeto 5</h1>
+      <UseEffect></UseEffect>
+    </>
+  )
+}
+
+export default App
