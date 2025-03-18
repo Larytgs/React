5c03e594d9608667ffec0c1ec6b7439d2ddfeeeb
laryTgs <laryssatortato99@gmail.com>
2025-03-18 11:09:45 -0300

    att

--- 8_ProjetoYoutube/src/App.jsx
+++ 8_ProjetoYoutube/src/App.jsx
@@ -1,7 +1,12 @@
+import AddTasks from "./componentes/AddTask";
+import Tasks from "./componentes/tasks";
+
 function App() {
   return (
     <>
-      <h1>Gerenciador de Tarefas</h1>
+      <h1 className="text-red-500 text-3xl">Gerenciador de Tarefas</h1>
+      <Tasks></Tasks>
+      <AddTasks></AddTasks>
     </>
   );
 }
