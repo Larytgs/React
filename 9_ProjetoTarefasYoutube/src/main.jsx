a9c25690103ee0387621f5059d145aa60bbdbc46
laryTgs <laryssatortato99@gmail.com>
2025-03-20 10:23:33 -0300

    criando paginas

--- 9_ProjetoTarefasYoutube/src/main.jsx
+++ 9_ProjetoTarefasYoutube/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TaskPage from "./paginas/TaskPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Ola</div>,
+  },
+  {
+    path: "/task", //o nome da pagina
+    element: <TaskPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
---
+++ 9_ProjetoTarefasYoutube/src/paginas/TaskPage.jsx
@@ -0,0 +1,6 @@
+function TaskPage() {
+  return;
+  <div className="space-y-3 p-6 bg-slate-200 shadow rounded-xl"></div>;
+}
+
+export default TaskPage;
